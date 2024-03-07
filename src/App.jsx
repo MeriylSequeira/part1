7bc3ff33ffb381071d6a9f0ad988de73e9e12433
Meriyl Freya Sequeira <23h32.meriyl@sjec.ac.in>
2024-03-07 15:00:31 +0530

    1.5 completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,35 +3,31 @@ import Content from "./components/Content";
 import Total from "./components/Total";
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header title={course} />
-      <Content 
-        part1={parts[0].name}
-        exercises1={pars[0].exercises} 
-        part2={parts[1].name} 
-        exercises2={parts[1].exercises} 
-        part3={parts[2].name} 
-        exercises3={parts[2].exercises}/>
-      <Total total={parts[0].exercises + parts[1].exercises + parts[2].exercises}/>
+      <Header course={course.name} />
+      <Content parts={course.parts}/>
+      <Total parts={course.parts} />
     </div>
-  );
-};
+  )
+}
 
 export default App;
